0d4c251d28688840f98a6f9382e1370ac7140c06
Christopher <schulze.christopher@icloud.com>
2025-05-19 15:49:21 +0200

    Delete .history directory

--- .history/build_20250515004750.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# QuicSand-VPN Server Build Script
-
-# Build the server
-go build -o quicsand-vpn cmd/main.go cmd/tun.go fec.go tetrys.go fake_tls.go config.go mtu_detection.go keepalive.go
-
-echo "QuicSand-VPN server binary created successfully"
